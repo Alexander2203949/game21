df8ab981ca0d5d34bcf112ab8fc42bb42165b2d1
nikitaRidinger <monster89161201337@gmail.com>
2023-09-05 20:20:21 +0300

    add_function_gen_chislo_comp

--- main copy.cpp
+++ main copy.cpp
@@ -4,16 +4,15 @@
 #include<time.h> //данная библиотека помогает рандому синхроризироваться по системному времени
 #include <string>
 #include <map>
-#include <windows.h>
-#include <list>
+
 using namespace std;
 namespace liter
 {
-    string y_number_is = "Your number is";
-    string summ_numbers = "Your summ number is";
-    string lose = "You LOSE";
-    string win = "You WIN!";
-    string new_or_stop = "Enter 'y' for new number or 'n' for stop game!";
+    std::string y_number_is = "Your number is";
+    std::string summ_numbers = "Your summ number is";
+    std::string lose = "You LOSE";
+    std::string win = "You WIN!";
+    std::string new_or_stop = "Enter 'y' for new number or 'n' for stop game!";
     string comp_number = "Computer number is";
     string comp_sum_numbers = "Computer summ number is";
     string comp_lose = "You LOSE";
@@ -23,24 +22,21 @@ namespace liter
     string check_player = "Enter 'c' if computer or 'p' second player";
 
 };//литеры вывода сообщений
-void gen_chislo_comp(vector<int> & summ_game_comp, int & sum_vec_comp);
 void gen_chislo(vector<int> & summ_game, int & sum_vec);
 void summ_chislo(vector<int> & summ_game, int & sum_vec);
-void summ_chislo_comp(vector<int> & summ_comp, int & sum_vec_comp);
 bool check_num(int & sum_vec);
 void print(string stroka,int num);//выводит 
 void print(string stroka);
 vector<int> summ_game; //храник числа выпавшие игроку
 int sum_vec = 0;//хранит сумму всех чисел из вектора
-vector<int>summ_comp;
-int sum_vec_comp = 0;
+
 
 
 int main()
 {
     srand((unsigned) time(NULL));
-    //gen_chislo(summ_game, sum_vec);
-    gen_chislo_comp(summ_comp,sum_vec_comp);
+    gen_chislo(summ_game, sum_vec);
+    
 
     return 0;
 
@@ -73,19 +69,8 @@ void gen_chislo(vector<int> & summ_game, int & sum_vec)
 
         }
     }
-}
-void gen_chislo_comp(vector<int> & summ_game_comp, int & sum_vec_comp) //создание рандомного числа для компьютера
-{
-    for(int i;i<4;i++)
-    {
-        int rand_chislo_comp = 0;
-        Sleep(250);
-        rand_chislo_comp = 2 + rand()%10;
-        summ_comp.push_back(rand_chislo_comp);
-        print(liter::comp_number,rand_chislo_comp);//вывод фразы Your number is и числадля компьютера
-        //print(liter::comp_sum_numbers,sum_vec_comp);//вывод фразы Your summ numbers is и общей суммы чисел для компьютера
-        
-    }
+
+
 }
 //эта функция суммирует числа в векторе
 void summ_chislo(vector<int> & summ_game, int & sum_vec)
