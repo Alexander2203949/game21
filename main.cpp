fc64b6176082f4dfa07c55b05266781dac4e51c0
Alexander2203949 <akuka89@mail.ru>
2023-09-05 20:57:50 +0300

    add code 2

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ namespace liter
 } //Литеры вывода сообщений
 
 void gen_chislo(vector<int> & summ_game, int & sum_vec);
+void gen_chislo_comp(vector<int> & summ_game_comp, int & sum_vec_comp);
 void summ_chislo(vector<int> & summ_game, int & sum_vec);
 void summ_comp_num(vector<int> & summ_comp, int & sum_vec_comp);
 bool check_num(int & sum_vec);
@@ -126,3 +127,17 @@ void summ_comp_num(vector<int> & summ_comp, int & sum_vec_comp)
         sum_vec_comp += number;
     }
 }//Эта функция суммирует числа компьютера в векторе.
+
+
+void gen_chislo_comp(vector<int> & summ_game_comp, int & sum_vec_comp)
+{
+    int rand_chislo_comp = 0; // получает рандомное число
+    for(int i = 0; i < 4; i++)
+    {
+        rand_chislo_comp = 2 + rand()%13;
+        summ_game_comp.push_back(rand_chislo_comp);
+        print(liter::comp_number, rand_chislo_comp);// вывод фразы "Your number is" и числа
+        Sleep(500);
+        //print(liter::comp_summ_numbers, sum_vec_comp); //вывод фразы "Your summ numbers is" и общей суммы
+    }
+}
